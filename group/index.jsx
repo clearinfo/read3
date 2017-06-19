00614a79e58a556899501d30fe3e650de2def9fa
clearinfo <songxian@163.com>
2017-06-19 17:56:14 +0800

    song

--- group/index.jsx
+++ group/index.jsx
@@ -161,7 +161,7 @@ class ZmitiGroupListApp extends Component {
 								<li><a href="javascript:void(0)" onTouchTap={this.provinceDialog.bind(this,3)}>3号悦读室</a></li>
 								<li><a href="javascript:void(0)" onTouchTap={this.provinceDialog.bind(this,4)}>4号悦读室</a></li>
 								<li><a href="javascript:void(0)" onTouchTap={this.provinceDialog.bind(this,5)}>5号悦读室</a></li>
-								<li><a href="javascript:void(0)" onTouchTap={this.provinceDialog.bind(this,6)}>6号悦读室</a></li>						
+														
 								<li><a href="javascript:void(0)" onTouchTap={this.provinceDialog.bind(this,7)}>人文大本营</a></li>
 								<li><a href="javascript:void(0)" onTouchTap={this.provinceDialog.bind(this,8)}>人文大本营II</a></li>
 								
@@ -183,13 +183,12 @@ class ZmitiGroupListApp extends Component {
 							<div className="zmiti-group-h3">励志类：</div>
 							<ul>
 								<li><a href="javascript:void(0)" onTouchTap={this.provinceDialog.bind(this,16)}>1号悦读室</a></li>
-								<li><a href="javascript:void(0)" onTouchTap={this.provinceDialog.bind(this,17)}>2号悦读室</a></li>						
+								<li><a href="javascript:void(0)" onTouchTap={this.provinceDialog.bind(this,17)}>励志大本营</a></li>						
 							</ul>
 							<div className="zmiti-group-h3">亲子类：</div>
-							<ul>
-								<li><a href="javascript:void(0)" onTouchTap={this.provinceDialog.bind(this,18)}>2号悦读室</a></li>						
-								<li><a href="javascript:void(0)" onTouchTap={this.provinceDialog.bind(this,19)}>3号悦读室</a></li>
-								
+							<ul>														
+								<li><a href="javascript:void(0)" onTouchTap={this.provinceDialog.bind(this,19)}>1号悦读室</a></li>
+								<li><a href="javascript:void(0)" onTouchTap={this.provinceDialog.bind(this,18)}>2号悦读室</a></li>
 								<li><a href="javascript:void(0)" onTouchTap={this.provinceDialog.bind(this,20)}>亲子大本营</a></li>
 								
 							</ul>
